c661c1e0a4e5322d2fff8e2e87871fd981ef57f6
Nikolas Gianoglou <nikolasgianoglou@Nikolass-MacBook-Air.local>
2024-02-28 15:02:59 -0300

    Fix problem when executing all tests, override methods setup/tearDown

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,12 +10,12 @@ import XCTest
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }
@@ -25,7 +25,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         let url = anyURL()
         let exp = expectation(description: "Wait for request")
-        
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
